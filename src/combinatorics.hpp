52ba384b5581e544b25796912d46d60dfac354b3
cyy <cyyever@outlook.com>
2019-11-23 23:40:21 +0800

    fix code

--- src/combinatorics.hpp
+++ src/combinatorics.hpp
@@ -1,13 +1,16 @@
 /*!
- * \file combinaiorics.hpp
+ * \file combinatorics.hpp
  *
  * \brief 包含组合学相关函数
  */
 #pragma once
 
 #include <cstdint>
+#include <memory>
+#include <range/v3/all.hpp>
 #include <vector>
 
+#include "exception.hpp"
 #include "integer.hpp"
 namespace cyy::math {
 
@@ -26,18 +29,43 @@ namespace cyy::math {
    *		n,k：参数
    *	返回值：
    *		第一个组合
-  std::vector<bool> my_next_combination(uint64_t n, uint64_t k);
    */
+  inline auto combination(uint64_t n, uint64_t k) {
+    if (k > n)
+      throw exception::out_of_range("k>n");
+    auto res = std::make_shared<std::vector<bool>>();
 
-  /*
-   *	功能：生成在n个数中选k个数的所有组合
-   *	参数：
-   *		n,k：参数
-   *		prev_combination：上一次调用此函数返回的组合
-   *	返回值：
-   *		返回下一个组合，如果组合已经全部生成完毕，则vector为空
-  std::vector<bool>
-  my_next_combination(uint64_t n, uint64_t k,
-                      const std::vector<bool> &prev_combination);
-   */
+    return ranges::views::generate([=]() {
+             if (res->empty()) {
+               res->resize(n, false);
+               for (uint64_t i = 0; i < k; i++)
+                 (*res)[i] = true;
+               return *res;
+             }
+             std::vector<bool>::size_type i, j, cnt;
+             uint8_t find_false;
+             cnt = 0;
+             find_false = 0;
+             i = n - 1;
+             while (true) {
+               if ((*res)[i] == true) {
+                 cnt++;
+                 (*res)[i] = false;
+                 if (find_false) {
+                   i++;
+                   for (j = 0; j < cnt; j++)
+                     (*res)[i + j] = true;
+                   return *res;
+                 }
+               } else
+                 find_false = 1;
+               if (i == 0)
+                 break;
+               i--;
+             }
+             res->resize(0);
+             return *res;
+           }) |
+           ranges::views::take_while([](auto const &v) { return !v.empty(); });
+  }
 } // namespace cyy::math
