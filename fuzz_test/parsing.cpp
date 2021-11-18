064d9378f1df9c92659d83eb3f0cc443a9f03515
cyy <cyyever@outlook.com>
2021-11-18 09:28:44 +0000

    fix code

--- fuzz_test/parsing.cpp
+++ fuzz_test/parsing.cpp
@@ -11,7 +11,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
     if (Size == 0) {
       return 0;
     }
-    cyy::math::integer(std::string(Data, Size));
+    cyy::math::integer(std::string(reinterpret_cast<const char*>Data, Size));
   } catch (const std::exception &) {
   }
   return 0; // Non-zero return values are reserved for future use.
