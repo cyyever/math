fd20f82fc91caeb4355aa27f21c39b3b2997c348
cyy <cyyever@outlook.com>
2021-11-18 09:23:14 +0000

    fix code

---
+++ fuzz_test/parsing.cpp
@@ -0,0 +1,18 @@
+/*!
+ * \file parsing.cpp
+ *
+ */
+
+#include "integer.hpp"
+using namespace cyy::math;
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+  try {
+    if (Size == 0) {
+      return 0;
+    }
+    cyy::math::integer(std::string(Data, Size));
+  } catch (const std::exception &) {
+  }
+  return 0; // Non-zero return values are reserved for future use.
+}
