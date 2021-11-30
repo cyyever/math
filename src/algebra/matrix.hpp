fd458d12d07c2312ce2df47ad4743db10b0e6fca
cyy <cyyever@outlook.com>
2021-11-30 20:48:25 +0800

    fix code

--- src/algebra/matrix.hpp
+++ src/algebra/matrix.hpp
@@ -10,8 +10,7 @@
 #include <Eigen/LU>
 namespace cyy::math::la {
   template <typename number_type = cyy::math::rational>
-  using matrix = Eigen::Matrix<number_type, cyy::math::rational, Eigen::Dynamic,
-                               Eigen::Dynamic>;
+  using matrix = Eigen::Matrix<number_type, Eigen::Dynamic, Eigen::Dynamic>;
   template <typename number_type = cyy::math::rational>
   using vector = Eigen::Matrix<number_type, Eigen::Dynamic, 1>;
 } // namespace cyy::math::la
